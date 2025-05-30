c07f276bdde75ef7b3e4402b4477864be2f8baea
DigvijaySinghRajput <rajputdd15@gmail.com>
2025-05-30 17:43:53 +0530

    redux toolkit

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authservice from "./appwrite/auth";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
 
-  return <></>;
+  useEffect(() => {
+    authservice
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Hea>
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
