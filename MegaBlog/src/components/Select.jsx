c6113ce0212e91d96ea6b34c8376128c898ea4e4
DigvijaySinghRajput <rajputdd15@gmail.com>
2025-06-11 23:59:09 +0530

    RTE Done

---
+++ MegaBlog/src/components/Select.jsx
@@ -0,0 +1,27 @@
+import React, { useId } from "react";
+
+const Select = React.forwardRef(function Select(
+  { lable, options, className = "", ...props },
+  ref
+) {
+  const Id = useId();
+  return (
+    <div className="w-full">
+      {lable && <label className="inline-block mb-1 pl-1" htmlFor={Id}></label>}
+      <select
+        {...props}
+        id={Id}
+        ref={ref}
+        className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full  ${className}`}
+      >
+        {options?.map((options) => (
+          <options key={options} value={options}>
+            {options}
+          </options>
+        ))}
+      </select>
+    </div>
+  );
+});
+
+export default Select;
--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,4 +1,9 @@
-import Footer from "./Footer/Footer";
-import Header from "./Header/header";
-
-export { Header, Footer };
+import footer from "./Footer/footer";
+import header from "./Header/header";
+import Container from "./container/Container";
+import logo from "./logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import RTE from "./RTE";
+import Select from "./Select";
+export { header, footer, Container, logo, LogoutBtn, Button, RTE, Select };
